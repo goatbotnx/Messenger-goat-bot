16a7416199791a29650fb42a71bcb5793e0b2e97
fakereactionx210-hub <fakereactionx210@gmail.com>
2025-10-07 07:04:56 +0600

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -20,10 +20,10 @@ module.exports = {
 
 	langs: {
 		vi: {
-			restartting: "🔄 | Đang khởi động lại bot..."
+			restartting: "🔄 | Đang khởi động lại bot...🥱"
 		},
 		en: {
-			restartting: "🔄 | Restarting bot..."
+			restartting: "🔄 | Restarting bot...🥱"
 		}
 	},
 
@@ -31,7 +31,7 @@ module.exports = {
 		const pathFile = `${__dirname}/tmp/restart.txt`;
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage(`✅ | Bot restarted\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
+			api.sendMessage(`✅ | Bot restarted\n😈 | Time: ${(Date.now() - time) / 1000}s`, tid);
 			fs.unlinkSync(pathFile);
 		}
 	},
